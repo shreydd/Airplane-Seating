b680773ceb3497a904b861ce2cc8ab616aaa2701
Shreyas <47713047+shreydd@users.noreply.github.com>
2022-12-15 14:41:48 +0000

    assign seats to available passengers

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 # seatsArray = input("input seats arrangement:")
 # no_of_passengers = input("input the no of passengers:")
 
-# columns, rows
 seatsArray = [[3,2], [4,3], [2,3], [3,4]]
+no_of_passengers = 30
 
 seats_id = []
 
 totalColumns = 0
 totalRows = 0
 
-no_of_passengers = 30
-
 aisleSeats = []
 windowSeats = []
 middleSeats = []
 
+# assign seats id for all available seats on the plane
 def cal_seats_id(totalColumns, totalRows):
   for (index, eachArray) in enumerate(seatsArray):
     columns = eachArray[0]
@@ -31,7 +30,8 @@ def cal_seats_id(totalColumns, totalRows):
 cal_seats_id(totalColumns, totalRows)
 
 
-def find_asile_seats(seatsArray):
+# find aisle seats
+def find_aisle_seats(seatsArray):
   rowCount = 0
   columnCount = 0
   for (index, eachArray) in enumerate(seatsArray):
@@ -69,11 +69,13 @@ def find_asile_seats(seatsArray):
     columnCount = columnCount + columns
         
 
-find_asile_seats(seatsArray)
+find_aisle_seats(seatsArray)
 
 # sort by row to go from left to right while assigning
 aisleSeats.sort(key=lambda x : x[1])
 
+
+# find window seats
 def find_window_seats(seatsArray):
   rowCount = 0
   columnCount = 0
@@ -113,17 +115,25 @@ seated_passenger = 0
 
 # assign seats 
 for eachEle in aisleSeats:
-  print("assignable aisle seat for", seated_passenger, "is",eachEle)
-  seated_passenger = seated_passenger + 1
+  if(seated_passenger <= no_of_passengers):
+    print("assignable aisle seat for", seated_passenger, "is",eachEle)
+    seated_passenger = seated_passenger + 1
 
 print(" ")
 
 for eachEle in windowSeats:
-  print("assignable window seat for", seated_passenger, "is", eachEle)
-  seated_passenger = seated_passenger + 1
+  if(seated_passenger <= no_of_passengers):
+    print("assignable window seat for", seated_passenger, "is", eachEle)
+    seated_passenger = seated_passenger + 1
 
 print(" ")
 
 for eachEle in middleSeats:
-  print("assignable middle seat for", seated_passenger, "is", eachEle)
-  seated_passenger = seated_passenger + 1
+  if(seated_passenger <= no_of_passengers):
+    print("assignable middle seat for", seated_passenger, "is", eachEle)
+    seated_passenger = seated_passenger + 1
+
+print(" ")
+
+if(no_of_passengers > seated_passenger):
+  print("The plane is at full capacity please wait for the next flight")
